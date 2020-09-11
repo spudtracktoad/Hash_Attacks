df28f8744a6116a7e160873947562f190ac346dc
Jared Smith <spudtracktoad@gmail.com>
2020-09-10 21:57:22 -0400

    Add project files.

---
+++ Hash_Attacks/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Attacks
+{
+    class Program
+    {
+        //Assumption: these are the only valid charaters
+        public static char[] validCharters = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                                         'A', 'B','C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        static void Main(string[] args)
+        {
+            int[] testSizes = new int[] { 8, 12, 16, 20 };
+            Dictionary<int, List<int>> CollisionDict = new Dictionary<int, List<int>>();
+            Dictionary<int, List<int>> PreImageDict = new Dictionary<int, List<int>>();
+            Dictionary<string, int> hashDict = new Dictionary<string, int>();
+            SimpleSHA1 testSha = new SimpleSHA1();
+            string baseCase = "testTEST";
+            Random random = new Random();
+
+            foreach (var item in testSizes)
+            {
+                hashDict.Clear();
+                var BaseResult = testSha.ComputeHash(item, baseCase);
+                List<int> collisionList = new List<int>();
+                List<int> preimageList = new List<int>();
+                for (int sample = 0; sample < 50; sample++)
+                {
+                    //preimage
+                    BitArray testResult;
+                    int count = 0;
+                    bool collision;
+                    do
+                    {
+                        collision = true;
+                        count++;
+                        char[] test = new char[item];
+                        for (int i = 0; i < item; i++)
+                        {
+                            test[i] = validCharters[random.Next(0, validCharters.Length)];
+                        }
+
+                        testResult = testSha.ComputeHash(item, new string(test));
+                        for (int d = 0; d < item; d++)
+                        {
+                            if (BaseResult.Get(d) != testResult.Get(d))
+                            {
+                                collision = false;
+                                break;
+                            }
+                        }
+                    } while (!collision);
+                    preimageList.Add(random.Next(0, count));
+                    count = 0;
+                    //Collision
+                    do
+                    {
+                        collision = false;
+                        count++;
+                        char[] test = new char[item];
+                        for (int i = 0; i < item; i++)
+                        {
+                            test[i] = validCharters[random.Next(0, validCharters.Length)];
+                        }
+                        var testStr = new string(test);
+                        testResult = testSha.ComputeHash(item, testStr);
+                        byte[] strArr = new byte[testResult.Length / 8];
+                        for (int i = 0; i < testResult.Length / 8; i++)
+                        {
+                            for (int index = i * 8, m = 1; index < i * 8 + 8; index++, m *= 2)
+                            {
+                                strArr[i] += testResult.Get(index) ? (byte)m : (byte)0;
+                            }
+                        }
+                        var hashStr = Encoding.ASCII.GetString(strArr);
+                        if (hashDict.ContainsKey(hashStr))
+                        {
+                            int tmpCount;
+                            hashDict.TryGetValue(hashStr, out tmpCount);
+                            count += tmpCount;
+                            hashDict[hashStr] = count;
+
+                            collision = true;
+                        }
+                        else
+                        {
+                            hashDict.Add(hashStr, count);
+                        }
+
+                    } while (!collision);
+                    collisionList.Add(count);
+                }
+                PreImageDict.Add(item, preimageList);
+                CollisionDict.Add(item, collisionList);
+            }
+
+            using (var writer = new StreamWriter(@"C:\Users\spudt\Documents\Fall 2020\Applied Cryptology\Labs\Hash_Attack\PreImageDict.csv"))
+            {
+                foreach (var pair in PreImageDict)
+                {
+                    writer.WriteLine("{0},{1};", pair.Key, String.Join(",", pair.Value.Select(x => x.ToString()).ToArray()));
+                }
+            }
+
+            using (var writer = new StreamWriter(@"C:\Users\spudt\Documents\Fall 2020\Applied Cryptology\Labs\Hash_Attack\CollisionDict.csv"))
+            {
+                foreach (var pair in CollisionDict)
+                {
+                    writer.WriteLine("{0},{1};", pair.Key, String.Join(",", pair.Value.Select(x => x.ToString()).ToArray()));
+                }
+            }
+
+            Console.WriteLine("start time: {0}", DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hash_Attacks/SimpleSHA1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Collections;
+
+namespace Hash_Attacks
+{
+    public class SimpleSHA1
+    {
+        private int Length;
+        private SHA1Managed sHA1Managed;
+
+        byte[] testHashInput = new byte[] { 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xa0, 0xb0, 0xc0, 0xd0, 0xe0, 0xf0 };
+        //byte[] HashInput = new byte[] { 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xa0, 0xb0, 0xc0, 0xd0, 0xe0, 0xf1 };
+
+        public SimpleSHA1()
+        {
+            sHA1Managed = new SHA1Managed();
+        }
+
+        public BitArray ComputeHash(int length, string input)
+        {
+            Length = length;
+            sHA1Managed.ComputeHash(Encoding.ASCII.GetBytes(input));
+            return buildHashOutput(sHA1Managed.Hash);
+        }
+
+        private BitArray buildHashOutput(byte[] input)
+        {
+            BitArray result = new BitArray(Length);
+            var test = new BitArray(input);
+                        
+            for (int i = 0; i < Length; i++)
+            {
+                result.Set(i, test.Get(i));
+            }
+
+            return result;
+        }
+
+        private String BitArrayToStr(BitArray ba)
+        {
+            byte[] strArr = new byte[ba.Length / 8];
+
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+
+            for (int i = 0; i < ba.Length / 8; i++)
+            {
+                for (int index = i * 8, m = 1; index < i * 8 + 8; index++, m *= 2)
+                {
+                    strArr[i] += ba.Get(index) ? (byte)m : (byte)0;
+                }
+            }
+
+            return encoding.GetString(strArr);
+        }
+
+        public int CountToNextCollision()
+        { return 0; }
+    }
+}
